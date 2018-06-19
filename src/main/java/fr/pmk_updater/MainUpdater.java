714db6fd69cc92b0b97e670d90d12c1b0febece8
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-19 23:35:26 +0200

    Ajout dev mod pour test des versions launchers non stable

--- src/main/java/fr/pmk_updater/MainUpdater.java
+++ src/main/java/fr/pmk_updater/MainUpdater.java
@@ -5,12 +5,25 @@ import fr.pmk_updater.utils.XmlChecker;
 
 public class MainUpdater {
 
+	public static boolean DEV_MODE;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		if(args.length != 1) {
+			DEV_MODE = false;
+		}else {
+			if(args[0] == "-dev" ) {
+				DEV_MODE = true;
+			}else {
+				DEV_MODE = false;
+			}
+		}
 		
 		ExceptionManager.setTitle(" Error box ");
 		
 		XmlChecker xmlChecker = new XmlChecker();
+		
 		
 		
 	}
