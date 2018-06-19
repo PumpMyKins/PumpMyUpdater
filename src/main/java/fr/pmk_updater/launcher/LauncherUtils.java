5494f51e24f8f92f01cbd759f805e3cf1ec6b130
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-20 01:57:20 +0200

    Fix checksum no file exception

--- src/main/java/fr/pmk_updater/launcher/LauncherUtils.java
+++ src/main/java/fr/pmk_updater/launcher/LauncherUtils.java
@@ -1,6 +1,11 @@
 package fr.pmk_updater.launcher;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 import fr.pmk_updater.MainUpdater;
 import fr.pmk_updater.utils.Utils;
@@ -20,6 +25,29 @@ public class LauncherUtils {
 		
 		
 		return false;
+	}
+	
+	public static String getChecksum(File f) {
+		
+		String md5 = null;
+		
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(f);
+			md5 = DigestUtils.md5Hex(fis);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			Utils.AddException(e);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Utils.AddException(e);
+			e.printStackTrace();
+		}
+		
+		return md5;
+		
 	}
 
 }
