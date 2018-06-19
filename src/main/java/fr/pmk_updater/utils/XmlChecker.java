0f46aa0bbe5735d9125862e68c2539cfc3500b25
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-20 00:21:25 +0200

    Fix getter dev & stable version

--- src/main/java/fr/pmk_updater/utils/XmlChecker.java
+++ src/main/java/fr/pmk_updater/utils/XmlChecker.java
@@ -5,17 +5,18 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
-import fr.pmk_updater.exception.ExceptionFile;
-import fr.pmk_updater.exception.ExceptionManager;
-
 public class XmlChecker {
 
-	private static String url = "";
+	private static String url = "http://launcher.pumpmykins.eu/launcher-jar-updater/pumpmyupdater.xml";
 	
 	public static Document buildXmlDocument(String url) {
 		
@@ -68,24 +69,116 @@ public class XmlChecker {
 		XmlChecker.url = url;
 	}
 	
+	public boolean isInit = false;
+	
 	private Document xmlDoc;
+	private ArrayList<VersionData> versionsList;
+	
+	private VersionData lastStableData;
+	private String lastStableVersion;
+	
+	private VersionData lastDevData;
+	private String lastDevVersion;
 	
 	public XmlChecker() {
 		
 		xmlDoc = buildXmlDocument(url);
 		
-		if(xmlDoc == null) {
+		if(xmlDoc != null) {
 			
-			Utils.pushException();
-			return;
-		}else {
-			
+			isInit = true;
 			// récupération methods réussie
+			lastDevVersion = getXmlLastDevVersion();
+			lastStableVersion = getXmlLastStableVersion();
+			
+			versionsList = getXmlVersions();
 			
 		}
 		
 	}
-
+	
+	public VersionData getLastVersionData(boolean b) {
+		
+		if(b) {
+			return getLastDevData();
+		}else {
+			return getLastStableData();
+		}
+		
+	}
+	
+	private ArrayList<VersionData> getXmlVersions(){
+		
+		ArrayList<VersionData> versions = new ArrayList<VersionData>();
+			
+		//On récupère la racine
+		Element racine = xmlDoc.getRootElement();
+		Element vRacine = racine.getChild("versions");
+			
+		//On liste toutes les versions
+		List<Element> listVersions = vRacine.getChildren("version");
+		Iterator<Element> iteratorVersions = listVersions.iterator();
+			
+		//On parcourt toutes les versions
+		while(iteratorVersions.hasNext()){
+			
+			Element version = (Element)iteratorVersions.next();
+				
+			Element elementNom = version.getChild("nom");
+			Element elementUrl = version.getChild("url");
+			
+			String versionName = elementNom.getText();
+			VersionData vData = new VersionData(versionName, elementUrl.getText());
+			
+			if(versionName.equals(lastDevVersion)) {
+				
+				//System.out.println("set dev " + vData);
+				setLastDevData(vData);
+				
+			}
+			
+			if(versionName.equals(lastStableVersion)) {
+				
+				//System.out.println("set stable " + vData);
+				setLastStableData(vData);
+				
+			}
+			
+			versions.add(vData);
+			
+		}
+		
+		return versions;
+	}
+	
+	private String getXmlLastDevVersion() {
+		
+		String v = "";
+		
+		Element racine = xmlDoc.getRootElement();
+		Element elementV = racine.getChild("last-dev");
+		
+		v = elementV.getText();
+		
+		//System.out.println("getXmlLastDevVersion() " + v);
+		return v;
+		
+	}
+	
+	private String getXmlLastStableVersion() {
+		
+		String v = "";
+		
+		Element racine = xmlDoc.getRootElement();
+		Element elementV = racine.getChild("last-stable");
+		
+		v = elementV.getText();
+		
+		//System.out.println("getXmlLastStableVersion() " + v);
+		return v;
+		
+	}
+	
 	
 	
 	public Document getXmlDoc() {
@@ -95,5 +188,47 @@ public class XmlChecker {
 	public void setXmlDoc(Document xmlDoc) {
 		this.xmlDoc = xmlDoc;
 	}
+
+	public VersionData getLastStableData() {
+		return lastStableData;
+	}
+
+	public void setLastStableData(VersionData lastStableData) {
+		this.lastStableData = lastStableData;
+	}
+
+	public VersionData getLastDevData() {
+		return lastDevData;
+	}
+
+	public void setLastDevData(VersionData lastDevData) {
+		this.lastDevData = lastDevData;
+	}
+
+	public ArrayList<VersionData> getVersionsList() {
+		return versionsList;
+	}
+
+	public void setVersionsList(ArrayList<VersionData> versionsList) {
+		this.versionsList = versionsList;
+	}
+
+	public String getLastDevVersion() {
+		return lastDevVersion;
+	}
+
+	public void setLastDevVersion(String lastDevVersion) {
+		this.lastDevVersion = lastDevVersion;
+	}
+
+	public String getLastStableVersion() {
+		return lastStableVersion;
+	}
+
+	public void setLastStableVersion(String lastStableVersion) {
+		this.lastStableVersion = lastStableVersion;
+	}
+	
+	
 	
 }
