effcdfc2b4f3f4a54b6cb9029045cfd8f3a5f35a
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-19 23:06:44 +0200

    Ajout methods utiles exception

--- src/main/java/fr/pmk_updater/utils/Utils.java
+++ src/main/java/fr/pmk_updater/utils/Utils.java
@@ -1,5 +1,8 @@
 package fr.pmk_updater.utils;
 
+import fr.pmk_updater.exception.ExceptionFile;
+import fr.pmk_updater.exception.ExceptionManager;
+
 public class Utils {
 
 	public static String getStringByStackTrace(StackTraceElement[] s) {
@@ -11,6 +14,21 @@ public class Utils {
 		}
 		
 		return trace;
+		
+	}
+	
+	public static void AddException(Exception e) {
+		
+		ExceptionManager.addError("[ " + e.getClass().toString() + " ] " + e.getMessage());
+		ExceptionFile.addError("[ " + e.getClass().toString() + " ] " + e.getMessage() + "\n" +Utils.getStringByStackTrace(e.getStackTrace()));
+		
+	}
+	
+	public static void pushException() {
+		
+		ExceptionManager.addError("\n Allez voir le fichier de logs pour plus d'informations");
+		ExceptionManager.show();
+		ExceptionFile.pushFile();
 		
 	}
 	
