7783b60ef870990e9aaa06b5efa1b725021c0520
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-20 01:32:28 +0200

    Fix JAR_NAME file & args equals methods & ajout validity checker & file downloader method

--- src/main/java/fr/pmk_updater/MainUpdater.java
+++ src/main/java/fr/pmk_updater/MainUpdater.java
@@ -1,31 +1,84 @@
 package fr.pmk_updater;
 
+import java.io.File;
+
 import fr.pmk_updater.exception.ExceptionManager;
+import fr.pmk_updater.launcher.LauncherUtils;
+import fr.pmk_updater.utils.Utils;
+import fr.pmk_updater.utils.VersionData;
 import fr.pmk_updater.utils.XmlChecker;
 
 public class MainUpdater {
 
 	public static boolean DEV_MODE;
+	public static String JAR_NAME = "launcher.jar";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		if(args.length != 1) {
 			DEV_MODE = false;
 		}else {
-			if(args[0] == "-dev" ) {
+			if(args[0].equals("-dev") ) {
 				DEV_MODE = true;
 			}else {
 				DEV_MODE = false;
 			}
 		}
 		
+		System.out.println("Etat du mode developpeur : " + DEV_MODE);
+		
 		ExceptionManager.setTitle(" Error box ");
 		
 		XmlChecker xmlChecker = new XmlChecker();
 		
+		if(!xmlChecker.isInit) {
+			
+			Utils.pushException();
+			return;
+			
+		}
 		
+		VersionData version = xmlChecker.getLastVersionData(DEV_MODE);
+		
+		System.out.println("Versions à téléchager : " + version);
+		
+		File file = new File("launcher.jar");
+		
+		if(!file.exists()) {
+			// launcher non existant
+			boolean isOk = LauncherUtils.download(version);
+			
+			if(!isOk) {
+				
+				Utils.pushException();
+				return;
+				
+			}
+			
+			// check de la file
+			
+			isOk = LauncherUtils.checkFile(version,file);
+			
+			if(!isOk) {
+				
+				Utils.pushException();
+				return;
+				
+			}
+			
+			// lancement du .jar
+			
+		}else {
+			
+			
+			
+		}
 		
 	}
 
+	private static boolean checkLauncherIsValid(File l) {
+		return DEV_MODE;
+		
+	}
+	
 }
