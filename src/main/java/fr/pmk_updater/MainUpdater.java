ec2820c38fad8612267a3c530fe0e13b2c844951
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-19 23:07:03 +0200

    Set du titre des dialogs box

--- src/main/java/fr/pmk_updater/MainUpdater.java
+++ src/main/java/fr/pmk_updater/MainUpdater.java
@@ -1,10 +1,16 @@
 package fr.pmk_updater;
 
+import fr.pmk_updater.exception.ExceptionManager;
+import fr.pmk_updater.utils.XmlChecker;
+
 public class MainUpdater {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		ExceptionManager.setTitle(" Error box ");
+		
+		XmlChecker xmlChecker = new XmlChecker();
 		
 		
 	}
