65d11de3f54885ed35140acdf308efe6bfb6d621
Clem-Fern <clement.coussieu@gmail.com>
2018-09-26 16:14:32 +0200

    Update interface graphique ( methodes utiles / getter/ setter )

--- src/main/java/fr/pmk_updater/gui/UpdaterFrame.java
+++ src/main/java/fr/pmk_updater/gui/UpdaterFrame.java
@@ -16,6 +16,11 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JProgressBar;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.UIManager;
+import javax.swing.plaf.basic.BasicProgressBarUI;
 
 public class UpdaterFrame extends JFrame {
 
@@ -25,12 +30,13 @@ public class UpdaterFrame extends JFrame {
 	private static final long serialVersionUID = -103233739386621286L;
 	private JPanel content;	
 	private JLabel loadind_gif;
-
+	private JLabel lblTlchargementDuLauncher;
+	
 	/**
 	 * Create the frame.
 	 * @throws IOException
 	 */
-	public UpdaterFrame() throws IOException {
+	public UpdaterFrame() throws Exception {
 		
 		///////////// FRAME SETUP ///////////////////////////////
 		
@@ -56,7 +62,7 @@ public class UpdaterFrame extends JFrame {
 		//JLabel label = new JLabel(getImageURL("http://launcher.pumpmykins.eu/launcher-jar-updater/ressources/pmk_logo_200x200.png"));
 		
 		JLabel logo_top = new JLabel(getImageURL("http://launcher.pumpmykins.eu/launcher-jar-updater/ressources/pmk_logo_200x200.png"));
-		logo_top.setBounds(50, 40, 200, 200);
+		logo_top.setBounds(50, 52, 200, 200);
 		content.add(logo_top);
 		
 		//////////////////////// WINDOWS SETUP ///////////////////////////////
@@ -67,18 +73,39 @@ public class UpdaterFrame extends JFrame {
 		content.add(windows);
 		windows.setLayout(null);
 		
+		////////////// LABEL UPDATER ////////////////////////////
+		
+		JLabel lblUpdatetext = new JLabel("PumpMyUpdater");
+		
+		lblUpdatetext.setForeground(Color.WHITE);
+		lblUpdatetext.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUpdatetext.setFont(new Font("Source Sans Pro", Font.BOLD, 36));
+		lblUpdatetext.setBounds(10, 102, 280, 46);
+		windows.add(lblUpdatetext);
+		
+		///////////////// STATE LABEL ///////////////////////////////////////////////////////////
+		
+		lblTlchargementDuLauncher = new JLabel("T\u00E9l\u00E9chargement du launcher.jar ...");
+		lblTlchargementDuLauncher.setFont(new Font("Source Sans Pro Black", Font.BOLD, 13));
+		lblTlchargementDuLauncher.setForeground(Color.WHITE);
+		lblTlchargementDuLauncher.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTlchargementDuLauncher.setBounds(20, 140, 270, 31);
+		windows.add(lblTlchargementDuLauncher);
+		
 		///////////////////////// LOADING GIF SETUP /////////////////////////////
 		
 		loadind_gif = new JLabel("");
-		setGIFAnim(true);
-		loadind_gif.setBounds(100, 151, 100, 197);
+		loadind_gif.setBounds(94, 120, 100, 228);
 		windows.add(loadind_gif);
+		
+		setGIFAnim(true);
+		
 		
 		////////// ACTIVE UPDATER GUI ///////////////
 		setVisible(true);
 	}
 	
-	private ImageIcon getImageURL(String u) throws IOException {
+	private ImageIcon getImageURL(String u) throws Exception {
 		
 			URL url = new URL(u);
 			BufferedImage img = ImageIO.read(url);
@@ -88,7 +115,7 @@ public class UpdaterFrame extends JFrame {
 		
 	}
 	
-	public void setGIFAnim(boolean b) throws IOException {
+	public void setGIFAnim(boolean b) throws Exception {
 		
 		if(b) {
 			
@@ -111,7 +138,19 @@ public class UpdaterFrame extends JFrame {
 	    int dx = centerPoint.x - windowSize.width / 2;
 	    int dy = centerPoint.y - windowSize.height / 2;    
 	    setLocation(dx, dy);
+				
+	}
+	
+	public void setUpdaterState(String t) {
 		
+		this.lblTlchargementDuLauncher.setText(t);
 		
 	}
+	
+	public void close() {
+		
+		new ClosingAnimateThread(this).start();
+		
+	}
+	
 }
